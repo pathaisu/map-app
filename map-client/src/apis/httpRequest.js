7a408674f19ca4f6d3605b4eaddd74433b86c41d
Pathai Suwannawiwat <pathaisu@gmail.com>
2020-10-31 14:48:52 +0700

    feat: add setResolve request

--- map-client/src/apis/httpRequest.js
+++ map-client/src/apis/httpRequest.js
@@ -14,6 +14,20 @@ export const getEvents = async (timestamp) => {
   return data;
 }
 
+export const setResolveEvent = async (event) => {
+  const data = await fetch('http://localhost:3002/map/v1/events/resolve', {
+    mode: 'cors',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(event),
+  }).then(res => res.json());
+
+  return data;
+}
+
+
 export const insertData = async (sensor) => {
   const data = await fetch('http://localhost:3002/map/v1/sensors', {
     mode: 'cors',
