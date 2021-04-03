21d432320b2af0ac5adb4108124704b4ce3e68ad
Pathai Suwannawiwat <pathaisu@gmail.com>
2021-04-04 00:50:39 +0700

    fix: update route index

--- map-server/routes/events/events.js
+++ map-server/routes/events/events.js
@@ -2,83 +2,14 @@ import getTime from 'date-fns/getTime/index.js';
 import differenceInSeconds from 'date-fns/fp/differenceInSeconds/index.js';
 
 
-const THRESHOLD = 60;
-
-export const eventConsumer = async (req, res) => {
-  const { collectionEvents } = req.app.locals;
-
-  let events = [];
-  let timestamp = getTime(new Date());
-  const queryTimestamp = req.query.timestamp;
-
-  // Query for events that haven't display on UI yet
-  if (queryTimestamp) {
-    events = await collectionEvents
-      .find({ 
-        $and: [
-          { timestamp: { $gt: `${queryTimestamp}` } },
-          { timestamp: { $ne: `${queryTimestamp}` } },
-        ]
-      })
-      .toArray()
-      .then(result => result);
-  }
-
-  res.json({
-    timestamp,
-    events,
-  });
-}
-
-export const eventProducer = async (req, res) => {
-  const { 
-    collectionWatcher,
-    collectionEvents,
- } = req.app.locals;
-
-  let timestamp = getTime(new Date());
-  
-  // 1. Find existing watcher then filter for invalid sensors (sensors that last timestamp is longer than THRESHOLD)
-  const watcher = await collectionWatcher
-    .find({})
-    .toArray()
-    .then(result => result);
-
-  const invalidWatcher = watcher.filter(data => {
-    const diffTime = differenceInSeconds(
-      getTime(+data.timestamp),
-      getTime(timestamp),
-    );
-
-    return diffTime > THRESHOLD;
-  });
-
-  // 2. Convert invalid watcher into new events then insert them to events collection
-  for (const sensor of invalidWatcher) {
-    timestamp = getTime(new Date());
-
-    // Some times payload contains `_id` but this property is not necessary for generating new event.
-    delete sensor._id;
-  
-    const event = {
-      eventType: 'polling',
-      reason: 'too long since last node',
-      timestamp: `${timestamp}`,
-      status: 'not_resolve',
-      sensor,
-    }
-
-    await collectionEvents.insertOne({ ...event });
-  }
-
-  res.json({ timestamp });
-}
+const THRESHOLD = process.env.THRESHOLD;
 
 export const alarmEventProducer = async (req, res) => {
   // Get mongo client from req.app.locals
   const { 
     collectionEvents, 
     collectionSensors, 
+    collectionWatcher,
   } = req.app.locals;
 
   const timestamp = getTime(new Date());
@@ -97,6 +28,16 @@ export const alarmEventProducer = async (req, res) => {
 
   await collectionSensors.insertOne({ ...sensor });
   await collectionEvents.insertOne({ ...event });
+
+  // Update current sensor to watcher
+  await collectionWatcher.update(
+    { id: sensor.id }, 
+    { 
+      ...sensor,
+      timestamp: `${timestamp}`,
+    },
+    { upsert: true }
+  );
 
   res.json(event);
 }
@@ -148,28 +89,6 @@ export const pollingEventProducer = async (req, res) => {
     },
     { upsert: true }
   );
-
-  res.json({ result: true });
-}
-
-export const setEventToResolve =  async (req, res) => {
-  // Get mongo client from req.app.locals
-  const { 
-    collectionEvents,
-  } = req.app.locals;
-
-  const events = JSON.parse(JSON.stringify(req.body));
-
-  // Update current sensor to watcher
-  events.map(async (event) => {
-    await collectionEvents.update(
-      { timestamp: event.timestamp }, 
-      { 
-        ...event,
-        status: 'resolve', 
-      },
-    );
-  });
 
   res.json({ result: true });
 }
--- map-server/routes/events/index.js
+++ map-server/routes/events/index.js
@@ -1,35 +1,52 @@
 import bodyParser from 'body-parser';
 import { 
-  eventConsumer,
-  eventProducer,
-  pollingEventProducer,
   alarmEventProducer,
-  setEventToResolve,
+  pollingEventProducer,
 } from './events.js';
+
+import { eventConsumer } from './handlers/consumer.js';
+import { eventProducer } from './handlers/producer.js';
+import { setEventToResolve } from './handlers/resolve.js';
 
 
 const jsonParser = bodyParser.json();
 
 export default (app) => {
-  // For front-end to fetch events 
-  app.get('/map/v1/events/consumer', 
-    (req, res) => eventConsumer(req, res),
-  );
-  // For corn-job to fetch & generate new events.
-  app.post('/map/v1/events/producer', 
-    (req, res) => eventProducer(req, res),
-  );
-  // To generate new polling event
+  /**
+   * To: fetch events by timestamp
+   * For: => front-end
+   */
+  app.get('/map/v1/events/consumer', (req, res) => eventConsumer(req, res));
+
+  /**
+   * To: fetch events
+   * To: generate new events
+   * For: => corn-job
+   */
+  app.post('/map/v1/events/producer', (req, res) => eventProducer(req, res));
+
+  /**
+   * To: generate new polling event
+   * For: => mqtt-service/utils/api
+   */
   app.post('/map/v1/events/polling', 
     jsonParser,
     (req, res) => pollingEventProducer(req, res),
   );
-  // To generate new alarm event
+
+  /**
+   * To: generate new alarm event
+   * For: => mqtt-service/utils/api
+   */ 
   app.post('/map/v1/events/alarm', 
     jsonParser,
     (req, res) => alarmEventProducer(req, res),
   );
-  // To update status of event to resolved
+
+  /** 
+   * To: update status of event to resolved
+   * For: => front-end to resolved event
+   */
   app.post('/map/v1/events/resolve', 
     jsonParser,
     (req, res) => setEventToResolve(req, res),
