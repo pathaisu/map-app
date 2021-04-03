1fe4e95c9cb7f6ea00197dcff38f3a19ecbdb9fb
Pathai Suwannawiwat <pathaisu@gmail.com>
2021-04-03 18:42:55 +0700

    fix: apply env to cron job api

--- map-cron-job/apis/httpRequest.js
+++ map-cron-job/apis/httpRequest.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 export const getEvents = async (timestamp) => {
-  const data = await fetch(`http://api:3002/map/v1/events/producer?timestamp=${timestamp}`, { 
+  const data = await fetch(`${process.env.API_URL}/map/v1/events/producer?timestamp=${timestamp}`, { 
     mode: 'cors',
     method: 'POST',
   }).then(res => res.json());
